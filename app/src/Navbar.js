6d9f622f9800e4c395ad9b9790b5e38d1217918f
Roger Mullins <roger.mullins@student.awesomeinc.org>
2022-10-14 17:11:44 +0000

    Pages Changing

--- app/src/Navbar.js
+++ app/src/Navbar.js
@@ -1,25 +1,21 @@
 function Navbar(props) {
 
-    const BreakfastClick = () => {};
-    const LunchClick = () => {};
-
     function handleClick(page) {
         props.pageUpdater(page);
     };
 
-
     return (
-        <div>
+        <div className="sticky-top text-bg-primary">
             <div>
                 <h1>GitGrub</h1>
             </div>
-            <button onClick={() => {handleClick(3)}} className="btn btn-danger">Breakfast</button>
-            <button onClick={() => {handleClick(4)}} className="btn btn-danger">Appetizers</button>
-            <button onClick={() => {handleClick(5)}} className="btn btn-danger">Brunch</button>
-            <button onClick={() => {handleClick(6)}} className="btn btn-danger">Lunch</button>
-            <button onClick={() => {handleClick(7)}} className="btn btn-danger">Dinner</button>
-            <button onClick={() => {handleClick(8)}} className="btn btn-danger">Desserts</button>
-            <button onClick={() => {handleClick(9)}} className="btn btn-danger">Sides</button>
+            <button onClick={() => {handleClick(2)}} className="btn btn-danger">Breakfast</button>
+            <button onClick={() => {handleClick(1)}} className="btn btn-danger">Appetizers</button>
+            <button onClick={() => {handleClick(3)}} className="btn btn-danger">Brunch</button>
+            <button onClick={() => {handleClick(4)}} className="btn btn-danger">Lunch</button>
+            <button onClick={() => {handleClick(5)}} className="btn btn-danger">Dinner</button>
+            <button onClick={() => {handleClick(7)}} className="btn btn-danger">Desserts</button>
+            <button onClick={() => {handleClick(6)}} className="btn btn-danger">Sides</button>
         </div>
     )
 };
---
+++ pseudocode.md
@@ -0,0 +1,13 @@
+# React Restaurant
+
+## Page States
+0. Splash Screen
+1. Appetizers
+1. Breakfast
+1. Brunch
+1. Lunch
+1. Dinner
+1. Sides
+1. Desserts
+1. Menu (All Categories)
+1. Specials
